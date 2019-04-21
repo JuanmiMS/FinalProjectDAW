beb0c666cc01f74bde6ed97a2a5000455dc9e6d6
Juan Miguel Mari <juanmi.marisanchez@gmail.com>
2019-04-21 19:40:12 +0200

    added login button

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+
+
+import GoogleLogin from 'react-google-login';
+
+class Login extends Component {
+
   render() {
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="App">
+        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+          <GoogleLogin
+            clientId="873955508498-k84pufrfv8gj39eqmkcvko9gud36gkql.apps.googleusercontent.com"
+            buttonText="LOGIN WITH GOOGLE"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+          />
       </div>
-    )
+    );
   }
 }
+
+export default Login;
