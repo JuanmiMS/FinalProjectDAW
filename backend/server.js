d7d3e176f95fefaa8adf8f98f06012b710c7e49f
Juan Miguel Mari <juanmi.marisanchez@gmail.com>
2019-04-21 20:18:49 +0200

    files moved

--- backend/server.js
+++ backend/server.js
@@ -84,10 +84,10 @@ router.post("/putData", (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
