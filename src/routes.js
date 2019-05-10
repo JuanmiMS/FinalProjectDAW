dec2a92041fb9f196edd5bab2d9e92ec7f30496b
Juan Miguel Mari <jmari@brujula.es>
2019-05-10 14:51:51 +0200

    added example template

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import Login from './pages/Login/login';
 import Home from './pages/Home/home';
+import Example from './pages/Example/example';
 
 let routing = (
     <Provider store={store}>
@@ -11,6 +12,7 @@ let routing = (
             <Switch>
                 <Route exact path="/" component={Login} />
                 <Route path="/home" component={Home} />
+                <Route path="/example" component={Example} />
             </Switch>
         </BrowserRouter>
     </Provider>
